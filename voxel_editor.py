ff001584c3ce5c48d2cf99c7e367679717a3c4df
Ye Kuang <yekuang.ky@gmail.com>
2022-03-06 15:15:36 +0800

    Stupid camera

--- voxel_editor.py
+++ voxel_editor.py
@@ -1,10 +1,8 @@
 import taichi as ti
-import os
-import time
+import numpy as np
+from renderer import Renderer
 
 ti.init(arch=ti.vulkan)
-
-from renderer import Renderer
 
 GRID_RES = 32
 SCREEN_RES = (640, 360)
@@ -15,7 +13,7 @@ renderer = Renderer(dx=1 / GRID_RES,
 window = ti.ui.Window("Voxel Editor", SCREEN_RES, vsync=True)
 selected_voxel_color = (0.2, 0.2, 0.2)
 
-SPP = 10
+SPP = 1
 
 def show_hud():
   global selected_voxel_color
@@ -25,18 +23,29 @@ def show_hud():
   window.GUI.end()
 
 def main():
-    canvas = window.get_canvas()
-    renderer.set_camera_pos(3.24, 1.86, -4.57)
+    last_camera_pos = np.array((3.24, 1.86, -4.57))
+    renderer.set_camera_pos(*last_camera_pos)
     renderer.floor_height[None] = -5e-3
-
     renderer.initialize_grid()
 
     total_voxels = renderer.total_non_empty_voxels()
+    last_mouse_pos = np.array(window.get_cursor_pos())
     print('Total nonempty voxels', total_voxels)
+    canvas = window.get_canvas()
     img = renderer.render_frame(spp=SPP)
 
     while window.running:
         show_hud()
+        if window.is_pressed(ti.ui.LMB):
+          mouse_pos = np.array(window.get_cursor_pos())
+          delta2d = mouse_pos - last_mouse_pos
+          delta = np.array([delta2d[0], delta2d[1], 0]) * 2
+          print(f'mouse_pos={mouse_pos} delta={delta} last_camera_pos={last_camera_pos}')
+          last_mouse_pos = mouse_pos
+          last_camera_pos += delta
+          renderer.set_camera_pos(*last_camera_pos)
+          renderer.reset()
+          img = renderer.render_frame(spp=SPP)
         canvas.set_image(img)
         window.show()
 
